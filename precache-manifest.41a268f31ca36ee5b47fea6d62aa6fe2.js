0fea36006ab4ff9b181113dc26883e5c06fbf55d
leonore14 <63911892+leonore14@users.noreply.github.com>
2020-08-29 16:22:06 +0200

    Updates

---
+++ precache-manifest.41a268f31ca36ee5b47fea6d62aa6fe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d689495752fa7b8a591e483a0237e39",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "c62e34ac8e5703d52aa5",
+    "url": "/weather-app/static/css/main.641409b0.chunk.css"
+  },
+  {
+    "revision": "017fc9dda7e2fa5e84b0",
+    "url": "/weather-app/static/js/2.35e17ddd.chunk.js"
+  },
+  {
+    "revision": "320addaff3e37295a01ab28fa4b66477",
+    "url": "/weather-app/static/js/2.35e17ddd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c62e34ac8e5703d52aa5",
+    "url": "/weather-app/static/js/main.55899c22.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  }
+]);
